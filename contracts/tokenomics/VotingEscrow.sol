2dbdb273dac2a52cf349e90229923c1360cb2152
0xMattAxel <>
2024-06-13 16:17:20 +0300

    token_addr param description updated KFI->KAI

--- contracts/tokenomics/VotingEscrow.sol
+++ contracts/tokenomics/VotingEscrow.sol
@@ -87,7 +87,7 @@ contract VotingEscrow is IERC721, IERC721Metadata, IVotes {
     uint internal tokenId;
 
     /// @notice Contract constructor
-    /// @param token_addr `KFI` token address
+    /// @param token_addr `KAI` token address
     constructor(address token_addr, address art_proxy) {
         token = token_addr;
         voter = msg.sender;
